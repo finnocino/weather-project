b6cf73bdd8f5e913df5d79fd6940730e0b0c8c01
Rita B <margarita.bern@gmail.com>
2024-05-30 00:28:01 -0400

    updated search engine js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert("Hello!!!!")
+function searchCity(event){
+    event.preventDefault();
+    let newCity = document.querySelector("#new-city");
+    //console.log(newCity.value);
+    let cityNameElement = document.querySelector("#city-name");
+    cityNameElement.innerHTML = newCity.value
+}
+let searchFormElement = document.querySelector("#search-form");
+//console.log(searchFormElement);
+searchFormElement.addEventListener("submit", searchCity);
